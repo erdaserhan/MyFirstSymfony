4fdf1466514c2b6c5dee09a4f7815696c999bd6c
erdaserhan <erhan209@gmail.com>
2024-08-30 14:44:06 +0200

    Controler myJson updated

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -4,12 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+//
+use Symfony\Component\HttpFoundation\Response;
+# use Symfony\Component\Routing\Attribute\Route;
 
 class FirstController extends AbstractController
 {
-    #[Route('/json', name: 'app_first')]
-    public function index(): JsonResponse
+
+    //Création de la méthode 
+    public function index(): Response
+    {
+        return new Response("<html>");
+    }
+
+
+    
+    //#[Route('/json', name: 'app_first')]
+    public function myJson(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
